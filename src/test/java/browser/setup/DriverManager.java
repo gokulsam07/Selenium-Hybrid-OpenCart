2f3e50e607e67fb342148b739b0dbf0a502cf6be
gokulsam07 <gokuls2381@gmail.com>
2024-02-03 23:47:33 +0530

    cleaned up

--- src/test/java/browser/setup/DriverManager.java
+++ src/test/java/browser/setup/DriverManager.java
@@ -1,8 +1,5 @@
 package browser.setup;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
